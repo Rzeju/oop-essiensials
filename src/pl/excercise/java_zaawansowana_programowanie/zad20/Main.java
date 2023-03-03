25eecafd66aaf464dbd43feda0b076e8c6772a4a
Piotr Andrzejak <pandrzejak93@gmail.com>
2023-03-03 16:16:41 +0100

    shapes

---
+++ src/pl/excercise/java_zaawansowana_programowanie/zad20/Main.java
@@ -0,0 +1,43 @@
+package pl.excercise.java_zaawansowana_programowanie.zad20;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Rectangle");
+        Rectangle rectangle = new Rectangle(10, 20);
+        System.out.println(rectangle.calculatePerimeter());
+        System.out.println(rectangle.calculateArea());
+
+        System.out.println("Triangle");
+        Triangle triangle = new Triangle(3, 4, 5);
+        System.out.println(triangle.calculatePerimeter());
+        System.out.println(triangle.calculateArea());
+
+        System.out.println("Hexagon");
+        Hexagon hexagon = new Hexagon(5);
+        System.out.println(hexagon.calculatePerimeter());
+        System.out.println(hexagon.calculateArea());
+
+        System.out.println("QUBE");
+        Qube qube = new Qube(4);
+        System.out.println(qube.calculatePerimeter());
+        System.out.println(qube.calculateArea());
+        System.out.println(qube.calculateVolume());
+
+        System.out.println("CONE");
+        Cone cone = new Cone(3, 5, 4);
+        System.out.println(cone.calculatePerimeter());
+        System.out.println(cone.calculateArea());
+        System.out.println(cone.calculateVolume());
+
+
+        //TRÓJKĄT DLA NIEPOPRAWNYCH DANYCH
+        //OBIEKT ZOSTANIE UTWORZONY NATOMIAST JEGO BOKI BEDE MIAŁY WARTOŚCI DOMYŚLNE
+        System.out.println("Bad Triangle");
+        Triangle badTriangle = new Triangle(1, 2, 10);
+        System.out.println(badTriangle);
+        System.out.println(badTriangle.calculatePerimeter());
+        System.out.println(badTriangle.calculatePerimeter());
+    }
+}
---
+++ src/pl/excercise/java_zaawansowana_programowanie/zad20/Shape.java
@@ -0,0 +1,8 @@
+package pl.excercise.java_zaawansowana_programowanie.zad20;
+
+public abstract class Shape {
+
+    public abstract double calculatePerimeter();
+
+    public abstract double calculateArea();
+}
