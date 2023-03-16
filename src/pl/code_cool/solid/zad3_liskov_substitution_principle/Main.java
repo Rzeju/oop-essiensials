0f8b889c70ab5ff6e845a2b77e212ff426a06592
Piotr Andrzejak <pandrzejak93@gmail.com>
2023-03-16 15:11:27 +0100

    solid - l

---
+++ src/pl/code_cool/solid/zad3_liskov_substitution_principle/Main.java
@@ -0,0 +1,18 @@
+package pl.code_cool.solid.zad3_liskov_substitution_principle;
+
+public class Main {
+
+    private static Rectangle getNewRectangle() {
+        return new Square();
+    }
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = Main.getNewRectangle();
+
+        rectangle.setWidth(5);
+        rectangle.setHeight(10);
+
+        System.out.println(rectangle.getArea());
+    }
+}
---
+++ src/pl/code_cool/solid/zad3_liskov_substitution_principle/Square.java
@@ -0,0 +1,17 @@
+package pl.code_cool.solid.zad3_liskov_substitution_principle;
+
+public class Square extends Rectangle {
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+        this.height = width;
+    }
+
+    public void setHeight(int height) {
+        this.width = height;
+        this.height = height;
+    }
+
+
+}
